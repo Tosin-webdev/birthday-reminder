e04e1f66789c73850da6a1e67a3d7a3d3a667e9a
Tosin-webdev <oladejit3@gmail.com>
2020-12-30 04:03:20 +0100

    first commit

---
+++ src/data.js
@@ -0,0 +1,44 @@
+export default [
+  {
+    id: 1,
+    name: "kyle pree",
+    age: 19,
+    image: "https://pbs.twimg.com/media/EMzidVNWoAEqmEY.jpg",
+  },
+
+  {
+    id: 2,
+    name: "hiller tilter",
+    age: 25,
+    image: "https://data.whicdn.com/images/237233167/original.jpg",
+  },
+  {
+    id: 3,
+    name: "sandra bancus",
+    age: 36,
+    image:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg",
+  },
+
+  {
+    id: 4,
+    name: "thomas bunday",
+    age: 34,
+    image:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg",
+  },
+  {
+    id: 5,
+    name: "skipper werner",
+    age: 29,
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQqbgU_NINhuqKJvYPo7IWK6RH70uIh4oHbvA&usqp=CAUhttps://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQqbgU_NINhuqKJvYPo7IWK6RH70uIh4oHbvA&usqp=CAU",
+  },
+  {
+    id: 6,
+    name: "frosh queen",
+    age: 42,
+    image:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg",
+  },
+];
